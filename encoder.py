0531d080dee2f54a5cf87a506328dd5b63b118fc
Amblora <59273361+Amblora@users.noreply.github.com>
2025-10-18 01:25:28 +0800

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -3,38 +3,36 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class EncoderLayer(nn.Module):
-    def __init__(self, attention, d_model, d_ff=None, series_decomp=0.1, dropout=0.1, activation="relu", use_apdc=True):
+    def __init__(self, attention, d_model, seq_len, d_ff=None, dropout=0.1, activation="relu", use_apdc=True):
         super(EncoderLayer, self).__init__()
         d_ff = d_ff or 4 * d_model
         self.attention = attention
         self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1, bias=False)
         self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1, bias=False)
-        self.decomp1 = fourier_decomp(series_decomp)
-        self.decomp2 = fourier_decomp(series_decomp)
         self.dropout = nn.Dropout(dropout)
-        self.activation = F.relu if activation == "relu" else F.gelu
+        self.activation = F.gelu if activation == "gelu" else F.relu # 推荐使用GELU
+
         self.use_apdc = use_apdc
-        self.apdc = AdaptiveFreqDenoiseBlock(dim=d_model)
-        
+        if self.use_apdc:
+            self.apdc = AdaptiveFreqDenoiseBlock(dim=d_model, seq_len=seq_len)
+            self.norm_apdc = nn.LayerNorm(d_model) 
+
         self.norm1 = nn.LayerNorm(d_model)  
-        self.norm2 = nn.LayerNorm(d_model) 
+        self.norm2 = nn.LayerNorm(d_model)  
 
     def forward(self, x, attn_mask=None):
-        new_x, attn = self.attention(x, x, x, attn_mask=attn_mask)
-        x = self.norm1(x + self.dropout(new_x))  
+        if self.use_apdc:
+            x = self.norm_apdc(x + self.apdc(x))
 
-        x, _ = self.decomp1(x)
+        attn_output, attn = self.attention(self.norm1(x), self.norm1(x), self.norm1(x), attn_mask=attn_mask)
+        x = x + self.dropout(attn_output)
 
-        y = x
+        y = self.norm2(x)
         y = self.dropout(self.activation(self.conv1(y.transpose(-1, 1))))
         y = self.dropout(self.conv2(y).transpose(-1, 1))
-        res, _ = self.decomp2(x + y)
-
-        if self.use_apdc:
-            res = self.apdc(res) 
-
-        return self.norm2(res), attn  
-
+        x = x + y
+        return x, attn 
+    
 
 class Encoder(nn.Module):
     def __init__(self, attn_layers, norm_layer=None):
